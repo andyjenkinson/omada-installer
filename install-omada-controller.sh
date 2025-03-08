3157fd52b9e7d35df83c478fb9d63c405b245e53
Andy Jenkinson <andyjenkinson@users.noreply.github.com>
2025-03-08 18:36:52 +0000

    Add debian 11
    Add Debian 11, with Mongo 7 and OpenJDK 17. Building JSVC from source

--- install-omada-controller.sh
+++ install-omada-controller.sh
@@ -27,12 +27,22 @@ echo "[+] Verifying supported OS"
 OS=$(hostnamectl status | grep "Operating System" | sed 's/^[ \t]*//')
 echo "[~] $OS"
 
+OsDist=ubuntu
+MongoRepo=multiverse
+Java=8
+JSVC=apt
 if [[ $OS = *"Ubuntu 20.04"* ]]; then
     OsVer=focal
 elif [[ $OS = *"Ubuntu 22.04"* ]]; then
     OsVer=jammy
+elif [[ $OS = *"Debian GNU/Linux 11"* ]]; then
+    OsVer=bullseye
+    OsDist=debian
+    MongoRepo=main
+    Java=17
+    JSVC=1.2.4
 else
-    echo -e "\e[1;31m[!] Script currently only supports Ubuntu 20.04 or 22.04! \e[0m"
+    echo -e "\e[1;31m[!] Script currently only supports Ubuntu 20.04 or 22.04, or Debian 11! \e[0m"
     exit
 fi
 
@@ -42,22 +52,52 @@ apt-get -qq install gnupg curl wget &> /dev/null
 
 echo "[+] Importing the MongoDB 7.0 PGP key and creating the APT repository"
 curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu $OsVer/mongodb-org/7.0 multiverse" > /etc/apt/sources.list.d/mongodb-org-7.0.list
+
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/$OsDist $OsVer/mongodb-org/7.0 $MongoRepo" > /etc/apt/sources.list.d/mongodb-org-7.0.list
 apt-get -qq update
 
 # Package dependencies
 echo "[+] Installing MongoDB 7.0"
-apt-get -qq install mongodb-org &> /dev/null
-echo "[+] Installing OpenJDK 8 JRE (headless)"
-apt-get -qq install openjdk-8-jre-headless &> /dev/null
+apt-get -qq install mongodb-org > /dev/null
+echo "[+] Installing OpenJDK $Java JRE (headless)"
+apt-get -qq install openjdk-${Java}-jre-headless > /dev/null
 echo "[+] Installing JSVC"
-apt-get -qq install jsvc &> /dev/null
 
-echo "[+] Downloading the latest Omada Software Controller package"
+# Install the package version always as the .deb depends on it
+apt-get -qq install jsvc > /dev/null
+if [ $JSVC != "apt" ]; then
+    apt-get -qq install autoconf make gcc > /dev/null
+    # Building JSVC required JDK not only JRE
+    echo "[+] Installing OpenJDK $Java JDK (headless)"
+    apt-get -qq install openjdk-${Java}-jdk-headless > /dev/null
+    echo "[+] Downloading JSVC ${JSVC} source"
+    wget -qP /tmp/ https://archive.apache.org/dist/commons/daemon/source/commons-daemon-${JSVC}-src.tar.gz
+
+    dir=`pwd`
+    cd /tmp
+    tar zxvf commons-daemon-${JSVC}-src.tar.gz > /dev/null
+
+    echo "[+] Building JSVC ${JSVC}"
+    cd commons-daemon-${JSVC}-src/src/native/unix
+    sh support/buildconf.sh > /dev/null
+    ./configure --with-java=/usr/lib/jvm/java-${Java}-openjdk-amd64  > /dev/null
+    make > /dev/null
+    cp -p jsvc /usr/bin/
+
+    cd $dir
+    rm -rf /tmp/commons-daemon-${JSVC}-src
+    rm /tmp/commons-daemon-${JSVC}-src.tar.gz
+fi
+
+echo "[+] Discovering the latest Omada Software Controller package"
 OmadaPackageUrl=$(curl -fsSL https://support.omadanetworks.com/us/product/omada-software-controller/?resourceType=download | grep -oPi '<a[^>]*href="\K[^"]*Linux_x64.deb[^"]*' | head -n 1)
+OmadaVersion=$(echo $(basename $OmadaPackageUrl) | tr "_" "\n" | sed -n '4p')
+echo "[+] Downloading Omada Software Controller $OmadaVersion"
 wget -qP /tmp/ $OmadaPackageUrl
-echo "[+] Installing Omada Software Controller $(echo $(basename $OmadaPackageUrl) | tr "_" "\n" | sed -n '4p')"
-dpkg -i /tmp/$(basename $OmadaPackageUrl) &> /dev/null
+
+echo "[+] Installing Omada Software Controller $OmadaVersion"
+dpkg -i /tmp/$(basename $OmadaPackageUrl) > /dev/null
+echo "$OmadaVersion" > /opt/tplink/EAPController/PACKAGE_VERSION.txt
 
 hostIP=$(hostname -I | cut -f1 -d' ')
 echo -e "\e[0;32m[~] Omada Software Controller has been successfully installed! :)\e[0m"
